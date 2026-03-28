c02aa6ca8d5d757ca78aa065800566cbb31e06c6
Paul Kirth <paulkirth@google.com>
2026-03-28 07:54:24 +0000

    [clang-doc] Move Info types into arenas
    Info types used to own significant chunks of data. As we move these into
    local arenas, these types must be trivially destructible, to avoid
    leaking resources when the arena is reset. Unfortunaly, there isn't a
    good way to transition all the data types one at a time, since most of
    them are tied together in some way. Further, as they're now allocated in
    the arenas, they often cannot be treated the same way, and even the
    aliases and interfaces put in pLace to simplify the transition cannot
    cover the full range of changes required.
    
    We also use some SFINAE tricks to avoid adding boilerplate for helper
    APIs, we'd otherwise ahve to support
    
    Though it introduces some additional churn, we also try to keep tests
    from using arena allocation as much as possible, since this is not
    required to test the implementation of the library. As much of the test
    code needed to be rewritten anyway, we take the opportunity to
    transition now.
    
    We see a large improvement in overall memory use, dropping by 15% over
    the previous patches, and taking the total savings down to almost 54%.
    Performance numbers stay mostly flat, but merging is vastly improved,
    seeing a reduction of 40+% over the previous patch, and a 90% reduction
    overall.
    
    | Metric | Baseline | Prev | This | Culm% | Seq% |
    | :--- | :--- | :--- | :--- | :--- | :--- |
    | Time | 920.5s | 1013.6s | 1014.5s | +10.2% | +0.1% |
    | Memory | 86.0G | 47.3G | 39.9G | -53.6% | -15.6% |
    
    | Benchmark | Baseline | Prev | This | Culm% | Seq% |
    | :--- | :--- | :--- | :--- | :--- | :--- |
    | BM_BitcodeReader_Scale/10 | 67.9us | 69.1us | 73.0us | +7.5% | +5.8% |
    | BM_BitcodeReader_Scale/10000 | 70.5ms | 24.5ms | 17.1ms | -75.8% | -30.4% |
    | BM_BitcodeReader_Scale/4096 | 23.2ms | 4.5ms | 6.5ms | -71.9% | +45.1% |
    | BM_BitcodeReader_Scale/512 | 509.4us | 540.3us | 850.1us | +66.9% | +57.4% |
    | BM_BitcodeReader_Scale/64 | 114.8us | 117.3us | 140.6us | +22.5% | +19.9% |
    | BM_EmitInfoFunction | 1.6us | 1.6us | 1.8us | +12.9% | +15.0% |
    | BM_Index_Insertion/10 | 2.3us | 4.2us | 4.1us | +79.5% | -1.1% |
    | BM_Index_Insertion/10000 | 3.1ms | 5.2ms | 5.4ms | +71.1% | +3.7% |
    | BM_Index_Insertion/4096 | 1.3ms | 2.1ms | 2.1ms | +64.9% | +2.0% |
    | BM_Index_Insertion/512 | 153.6us | 244.7us | 252.9us | +64.7% | +3.3% |
    | BM_Index_Insertion/64 | 18.1us | 29.1us | 30.4us | +68.3% | +4.4% |
    | BM_JSONGenerator_Scale/10 | 36.8us | 38.3us | 38.1us | +3.5% | -0.4% |
    | BM_JSONGenerator_Scale/10000 | 89.6ms | 90.8ms | 84.2ms | -6.0% | -7.2% |
    | BM_JSONGenerator_Scale/4096 | 33.7ms | 35.9ms | 32.1ms | -4.7% | -10.5% |
    | BM_JSONGenerator_Scale/512 | 1.9ms | 2.0ms | 2.0ms | +3.1% | +0.4% |
    | BM_JSONGenerator_Scale/64 | 222.4us | 231.9us | 232.5us | +4.5% | +0.2% |
    | BM_Mapper_Scale/10000 | 104.3ms | 104.9ms | 102.1ms | -2.1% | -2.7% |
    | BM_Mapper_Scale/4096 | 44.3ms | 44.4ms | 46.5ms | +4.9% | +4.9% |
    | BM_Mapper_Scale/512 | 7.6ms | 7.6ms | 7.4ms | -2.1% | -1.8% |
    | BM_Mapper_Scale/64 | 3.1ms | 3.0ms | 3.0ms | -1.3% | -0.0% |
    | BM_MergeInfos_Scale/10000 | 12.2ms | 1.9ms | 931.2us | -92.4% | -49.8% |
    | BM_MergeInfos_Scale/2 | 1.9us | 1.7us | 1.8us | -4.5% | +4.7% |
    | BM_MergeInfos_Scale/4096 | 2.8ms | 521.8us | 201.3us | -92.8% | -61.4% |
    | BM_MergeInfos_Scale/512 | 68.9us | 34.9us | 19.9us | -71.2% | -43.1% |
    | BM_MergeInfos_Scale/64 | 10.3us | 5.8us | 4.1us | -59.8% | -28.4% |
    | BM_MergeInfos_Scale/8 | 2.8us | 2.1us | 1.9us | -33.3% | -12.6% |
    | BM_SerializeFunctionInfo | 25.5us | 26.0us | 26.2us | +2.4% | +0.8% |

--- clang-tools-extra/clang-doc/Representation.cpp
+++ clang-tools-extra/clang-doc/Representation.cpp
@@ -105,17 +105,6 @@ static llvm::Expected<OwnedPtr<Info>> reduce(OwningPtrArray<Info> &Values) {
   return std::move(Merged);
 }
 
-// Return the index of the matching child in the vector, or -1 if merge is not
-// necessary.
-template <typename T>
-static int getChildIndexIfExists(OwningVec<T> &Children, T &ChildToMerge) {
-  for (unsigned long I = 0; I < Children.size(); I++) {
-    if (ChildToMerge.USR == Children[I].USR)
-      return I;
-  }
-  return -1;
-}
-
 template <typename T>
 static void reduceChildren(llvm::simple_ilist<T> &Children,
                            llvm::simple_ilist<T> &&ChildrenToMerge) {
@@ -133,24 +122,13 @@ static void reduceChildren(llvm::simple_ilist<T> &Children,
   }
 }
 
-template <typename T>
-static void reduceChildren(OwningVec<T> &Children,
-                           OwningVec<T> &&ChildrenToMerge) {
-  for (auto &ChildToMerge : ChildrenToMerge) {
-    int MergeIdx = getChildIndexIfExists(Children, ChildToMerge);
-    if (MergeIdx == -1) {
-      Children.push_back(std::move(ChildToMerge));
-      continue;
-    }
-    Children[MergeIdx].merge(std::move(ChildToMerge));
-  }
-}
-
 template <typename Container>
 static void mergeUnkeyed(Container &Target, Container &&Source) {
-  for (auto &Item : Source) {
+  while (!Source.empty()) {
+    auto &Item = Source.front();
+    Source.pop_front();
     if (llvm::none_of(Target, [&](const auto &E) { return E == Item; }))
-      Target.push_back(std::move(Item));
+      Target.push_back(Item);
   }
 }
 
@@ -484,8 +462,8 @@ bool Index::operator<(const Index &Other) const {
   return Name < Other.Name;
 }
 
-OwningVec<const Index *> Index::getSortedChildren() const {
-  OwningVec<const Index *> SortedChildren;
+std::vector<const Index *> Index::getSortedChildren() const {
+  std::vector<const Index *> SortedChildren;
   SortedChildren.reserve(Children.size());
   for (const auto &[_, C] : Children)
     SortedChildren.push_back(&C);
@@ -527,22 +505,26 @@ ClangDocContext::ClangDocContext(tooling::ExecutionContext *ECtx,
   }
 }
 
-void ScopeChildren::sort() {
-  std::vector<Reference *> V;
-  while (!Namespaces.empty()) {
-    V.push_back(&Namespaces.front());
-    Namespaces.pop_front();
-  }
-  llvm::sort(V, [](const Reference *A, const Reference *B) { return *A < *B; });
-  for (auto *R : V)
-    Namespaces.push_back(*R);
+template <typename T> static void sortOwningVec(OwningVec<T> &List) {
+  if (List.empty() || std::next(List.begin()) == List.end())
+    return;
+  std::vector<T *> Sorted;
+  for (auto &Item : List)
+    Sorted.push_back(&Item);
+  llvm::sort(Sorted, [](const T *A, const T *B) { return *A < *B; });
+  List.clear();
+  for (auto *Item : Sorted)
+    List.push_back(*Item);
+}
 
-  llvm::sort(Records);
-  llvm::sort(Functions);
-  llvm::sort(Enums);
-  llvm::sort(Typedefs);
-  llvm::sort(Concepts);
-  llvm::sort(Variables);
+void ScopeChildren::sort() {
+  sortOwningVec(Namespaces);
+  sortOwningVec(Records);
+  sortOwningVec(Functions);
+  sortOwningVec(Enums);
+  sortOwningVec(Typedefs);
+  sortOwningVec(Concepts);
+  sortOwningVec(Variables);
 }
 } // namespace doc
 } // namespace clang
